473ef132cd042af1455840306a5cc6fa765f7e15
Scott <treii28@gmail.com>
2022-03-16 12:24:01 -0400

    added child-scope loader for injecting jquery backward from require.js scope. (as opposed to having require.js wait for global scope)

--- app/jqueryLoader.js
+++ app/jqueryLoader.js
@@ -1,11 +1,21 @@
 // don't load jquery if it is already loaded
 //   use a promise so [second] requirejs loading completes before proceeding
 new Promise((resolve, reject) => {
+    console.log("in jqueryLoader Promise")
     if(typeof($) == 'function') {
+        console.log("jquery is loaded in Promise", setJqueryGlobal)
+        if(typeof(setJqueryGlobal) === 'function') setJqueryGlobal($);
         // define 'jquery' so requirejs knows how to get it
-        define('jquery', [], function() { resolve($) });
+        define('jquery', [], function() {
+            resolve($)
+        });
     } else {
+        console.log("jquery is NOT loaded in Promise")
         // require jquery so it's available to requirejs
-        requirejs(['jquery'], function( $ ) { resolve($) });
+        requirejs(['jquery'], function( $ ) {
+            console.log("in Promise requirejs block",setJqueryGlobal)
+            if(typeof(setJqueryGlobal) === 'function') setJqueryGlobal($);
+            resolve($)
+        });
     }
 }).then( ($) => { return $ } )
